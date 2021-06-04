8f11130db79c0a7bcb6537fa314e8f9289c421eb
Ilyenkov <a.ilyenkov@mail.ru>
2021-06-04 16:49:02 +0300

    init project

---
+++ src/main/java/com/example/postswebapi/PostsWebApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.postswebapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostsWebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostsWebApiApplication.class, args);
+	}
+
+}
